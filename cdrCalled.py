2ff7b4b47cb736b7ed941991f8e2fde559b74e76
Aaron McMillan <aamcmillan@gmail.com>
2022-03-16 10:47:37 -0700

    updates

--- cdrCalled.py
+++ cdrCalled.py
@@ -8,54 +8,61 @@ print('  2. Rename your CDR data file to "cdr.csv"\n')
 print('SEARCHING INSTRUCTIONS:')
 print('  It is important NOT to include - or . characters in your number search.')
 print('  For example searching for 208-625-5555... you would use 2086255555.\n')
+print('  To search for more than one number, use a comma seperate list.')
+print('  Example: 2086251234,2086252345,2086253456\n')
 print('---------------------')
 
 continueLookup = True
 while(continueLookup):
     # Gather number(s) to search for.
-    calledNum = input('Please enter the called number in the format XXXXXXXXXX:')
+    callNum = []
+    uNumLookup = input('\nPlease enter the called number in the format XXXXXXXXXX:')
+    callNum = uNumLookup.split(',')
 
     csvFile = "exportData/cdr.csv"
     csvData = pd.read_csv(csvFile, low_memory=False)
 
-    # Search through CSV data for called party number.
-    result = csvData[csvData['finalCalledPartyNumber']==calledNum]
+    for num in callNum:
+        num = str(num)
+        # Search through CSV data for called party number.
+        result = csvData[csvData['finalCalledPartyNumber']==num]
 
-    # Strip all columns except the following
-    result = result[['dateTimeOrigination', 'callingPartyNumber', 'finalCalledPartyNumber']]
-    result['dateTimeOrigination'] = pd.to_datetime(result['dateTimeOrigination'], unit='s', origin='unix')
-    totalCallsSum = result.shape[0]
+        # Strip all columns except the following
+        result = result[['dateTimeOrigination', 'callingPartyNumber', 'finalCalledPartyNumber']]
+        result['dateTimeOrigination'] = pd.to_datetime(result['dateTimeOrigination'], unit='s', origin='unix')
+        totalCallsSum = str(result.shape[0])
 
-    print('\n')
-    print('----     CDR RESULTS     ----')
-    print('The called number ' + str(calledNum) + ' has been called ' + str(totalCallsSum) + ' times.\n')
-    print('---- TOTAL CALLS PER-DAY ----')
-    print('dateTime, totalCalls')
+        print('\n')
+        print('----     CDR RESULTS     ----')
+        print('The called number ' + num + ' has been called ' + totalCallsSum + ' times.\n')
+        print('---- TOTAL CALLS PER-DAY ----')
+        print('dateTime, totalCalls')
 
-    #Calculate the total calls for the called number by date
-    datez = pd.to_datetime(result['dateTimeOrigination'], unit='s', origin='unix')
-    totalCalls = datez.groupby(datez.dt.floor('d')).size().reset_index(name='count')
+        #Calculate the total calls for the called number by date
+        datez = pd.to_datetime(result['dateTimeOrigination'], unit='s', origin='unix')
+        totalCalls = datez.groupby(datez.dt.floor('d')).size().reset_index(name='count')
 
-    for date in totalCalls.index:
-        todaysDate = totalCalls['dateTimeOrigination'][date]
-        todaysCount = totalCalls['count'][date]
-        print(todaysDate.strftime('%Y-%m-%d') + ', ' + str(todaysCount))
+        for date in totalCalls.index:
+            todaysDate = totalCalls['dateTimeOrigination'][date]
+            todaysCount = totalCalls['count'][date]
+            print(todaysDate.strftime('%Y-%m-%d') + ', ' + str(todaysCount))
 
-    uInput = False
-    x = input('\nWould you like to see all calls? (y/n) ').lower()
-    if x.startswith('y'): uInput = True
+        uInput = False
+        x = input('\nWould you like to see all calls? (y/n) ').lower()
+        if x.startswith('y'): uInput = True
 
-    if uInput:
-        print('\n----  INDIVIDUAL CALLS   ----')
-        print('dateTime, calling, called')
+        if uInput:
+            print('\n----  INDIVIDUAL CALLS   ----')
+            print('dateTime, calling, called')
 
-        for call in result.index:
-            calling = result['callingPartyNumber'][call]
-            called = result['finalCalledPartyNumber'][call]
-            time = result['dateTimeOrigination'][call]
-            print(str(time) + ', ' + calling + ', ' + called)
+            for call in result.index:
+                calling = result['callingPartyNumber'][call]
+                called = result['finalCalledPartyNumber'][call]
+                time = result['dateTimeOrigination'][call]
+                print(str(time) + ', ' + calling + ', ' + called)
     
+    continueLookup = False
     ctLookupInput = input('\n\nWould you like to lookup another number? (y/n) ').lower()
-    if ctLookupInput.startswith('n'): continueLookup = False
+    if ctLookupInput.startswith('y'): continueLookup = True
 
 print('\n\nComplete...\n')
